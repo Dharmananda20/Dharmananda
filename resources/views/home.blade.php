39240f5f8738800a198dd7b0fb6a3dace140218c
MarioAldyWijaya <dharmananda_2327240078@mhs.mdp.ac.id>
2025-06-08 19:13:37 +0700

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h1>Data Mahasiswa</h1>
+    <p><strong>NPM: 2327240078</p>
+    <p><strong>Nama: Dharmananda</p>
+</body>
+</html>
